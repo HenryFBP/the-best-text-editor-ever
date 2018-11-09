8a904170950a2cc4262ef1abfdfa939a8ec7efd8
Henry Post <HenryFBP@gmail.com>
2018-11-09 11:54:25 -0600

    fix MenuActivity

--- the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/MenuActivity.java
+++ the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/MenuActivity.java
@@ -1,6 +1,10 @@
 package cooldomainname.com.the_best_text_editor_ever;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,46 +14,58 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MenuActivity {
-
-    spinner1 = (Spinner) findViewById(R.id.spinner1);
-    spinner1.setOnItemSelectedListener(this);
-    List<String> menu = new ArrayList<String>();
-    menu.add("Save");
-    menu.add("Rename");
-
-
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, menu);
-    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    spinner1.setAdapter(adapter);
-
-
-    spinner2 = (Spinner) findViewById(R.id.spinner2);
-    spinner2.setOnItemSelectedListener(this);
-    List<String> edit = new ArrayList<String>();
-    edit.add("Cut");
-    edit.add("Select");
-    edit.add("Move");
-
-
-    ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, edit);
-    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    spinner_2.setAdapter(adapter2);
-
-
-
+public class MenuActivity extends AppCompatActivity {
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position,
-                               long id) {
-        // TODO Auto-generated method stub
-        Toast.makeText(this, "YOUR SELECTION IS : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cool_ui);
+
+        Spinner spinner1 = findViewById(R.id.spinner1);
+
+        Log.i(MenuActivity.class.getSimpleName(), spinner1.toString());
+
+        List<String> menu = new ArrayList<>();
+        menu.add("Save");
+        menu.add("Rename");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, menu);
+        List<String> edit = new ArrayList<>();
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "YOUR SELECTION IS : " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
 
 
-    }
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, edit);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        edit.add("Cut");
+        edit.add("Select");
+        edit.add("Move");
+
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
 
     }
 
