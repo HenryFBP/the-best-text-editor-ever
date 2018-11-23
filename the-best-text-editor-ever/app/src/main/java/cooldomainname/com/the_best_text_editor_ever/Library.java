90cac1bb6ab8f1da1849e73251a6349f9cd7b4c3
Henry Post <HenryFBP@gmail.com>
2018-11-23 15:30:49 -0600

    add bread-toasting library

---
+++ the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/Library.java
@@ -0,0 +1,21 @@
+package cooldomainname.com.the_best_text_editor_ever;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Library {
+
+    public static void toastLong(CharSequence charSequence, Context c) {
+        toast(charSequence, c, Toast.LENGTH_LONG);
+    }
+
+    public static void toastShort(CharSequence charSequence, Context c) {
+        toast(charSequence, c, Toast.LENGTH_SHORT);
+    }
+
+    public static void toast(CharSequence charSequence, Context c, int length) {
+        Toast.makeText(c, charSequence.toString(), length).show();
+    }
+
+
+}
