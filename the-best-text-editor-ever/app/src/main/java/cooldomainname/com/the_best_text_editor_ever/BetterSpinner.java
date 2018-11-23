4f8bc09efc27ede73501de42664ca00cf73c323c
Henry Post <HenryFBP@gmail.com>
2018-11-22 22:16:18 -0600

    fix spinners being triggered by view initializing

---
+++ the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/BetterSpinner.java
@@ -0,0 +1,24 @@
+package cooldomainname.com.the_best_text_editor_ever;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+//TODO finish this class and move that boolean var here.
+
+public class BetterSpinner extends android.support.v7.widget.AppCompatSpinner {
+    public BetterSpinner(Context context) {
+        super(context);
+    }
+
+    public BetterSpinner(Context context, int mode) {
+        super(context, mode);
+    }
+
+    public BetterSpinner(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BetterSpinner(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
--- the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/TextEditActivity.java
+++ the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/TextEditActivity.java
@@ -23,13 +23,19 @@ public class TextEditActivity extends AppCompatActivity {
     /**
      * Actions you can perform on a file.
      */
-    private List<String> listFileActions = Arrays.asList("save", "rename");
+    private List<String> listFileActions = Arrays.asList("", "save", "rename");
 
     /**
      * Actions you can perform on text.
      */
-    private List<String> listTextActions = Arrays.asList("cut", "select", "move");
+    private List<String> listTextActions = Arrays.asList("", "cut", "select", "move");
 
+    /**
+     * We want to save the contents of our {@link TextEditActivity#textBuffer} to a file.
+     */
+    private void saveFile() {
+        //TODO do it!
+    }
 
     /**
      * When this Activity is created.
@@ -48,67 +54,85 @@ public class TextEditActivity extends AppCompatActivity {
 
         }
 
-        Spinner spinnerFileActions = findViewById(R.id.spinnerFileActions);
+        // Spinner for actions that can be performed on a file.
+        final Spinner spinnerFileActions = findViewById(R.id.spinnerFileActions);
         ArrayAdapter<String> adapterFileActions = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listFileActions);
         adapterFileActions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerFileActions.setAdapter(adapterFileActions);
+        final boolean[] spinnerFileActionsSelected = {false}; //TODO move these to the class level.
 
+        spinnerFileActions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                //Prevents initialization from triggering this {@link ArrayAdapter.onItemSelected} event.
+                if (spinnerFileActionsSelected[0]) {
+
+                    String selection = ((String) parent.getItemAtPosition(position));
+
+                    Toast.makeText(getApplicationContext(), String.format("Your file action selection is '%s'.", selection), Toast.LENGTH_SHORT).show();
+
+                    switch (selection.toLowerCase()) {
+                        case "save": {
+                            saveFile();
+                            break;
+                        }
+
+                        case "rename": {
+                            break;
+                        }
+
+                        default: {
+                            break;
+                        }
+                    }
+                } else {
+                    spinnerFileActionsSelected[0] = true;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+        spinnerFileActions.setSelection(-1);
+
+        // Spinner for actions that can be performed on text.
         Spinner spinnerTextActions = findViewById(R.id.spinnerTextActions);
         ArrayAdapter<String> adapterTextActions = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listTextActions);
         adapterTextActions.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerTextActions.setAdapter(adapterTextActions);
-
-        spinnerFileActions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-                String selection = ((String) parent.getItemAtPosition(position));
-
-                Toast.makeText(getApplicationContext(), String.format("Your file action selection is '%s'.", selection), Toast.LENGTH_SHORT).show();
-
-
-                switch (selection.toLowerCase()) {
-                    case "save": {
-                        break;
-                    }
-
-                    case "rename": {
-                        break;
-                    }
-
-                    default: {
-                        break;
-                    }
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                // TODO Auto-generated method stub
-            }
-        });
-
+        final boolean[] spinnerTextActionsSelected = {false};
 
         spinnerTextActions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selection = ((String) parent.getItemAtPosition(position));
 
-                Toast.makeText(getApplicationContext(), String.format("Your text editing selection is '%s'.", selection), Toast.LENGTH_SHORT).show();
+                //Prevents initialization from triggering this {@link ArrayAdapter.onItemSelected} event.
+                if (spinnerTextActionsSelected[0]) {
+                    String selection = ((String) parent.getItemAtPosition(position));
 
-                switch (selection.toLowerCase()) {
-                    case "cut": {
-                        break;
+                    Toast.makeText(getApplicationContext(), String.format("Your text editing selection is '%s'.", selection), Toast.LENGTH_SHORT).show();
+
+                    switch (selection.toLowerCase()) {
+                        case "cut": {
+                            break;
+                        }
+                        case "select": {
+                            break;
+                        }
+                        case "move": {
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
                     }
-                    case "select": {
-                        break;
-                    }
-                    case "move": {
-                        break;
-                    }
-                    default: {
-                        break;
-                    }
+                } else {
+                    spinnerTextActionsSelected[0] = true;
                 }
             }
 
@@ -117,6 +141,7 @@ public class TextEditActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
             }
         });
+        spinnerTextActions.setSelection(-1);
 
     }
 
