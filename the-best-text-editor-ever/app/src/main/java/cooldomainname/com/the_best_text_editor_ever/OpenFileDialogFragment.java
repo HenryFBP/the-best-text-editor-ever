53f47ec29ff6ff4ed0cdeae44a3740c07b9fed3e
Henry Post <HenryFBP@gmail.com>
2018-11-25 14:44:07 -0600

    add listener and menu item for opening a file

--- the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/OpenFileDialogFragment.java
+++ the-best-text-editor-ever/app/src/main/java/cooldomainname/com/the_best_text_editor_ever/OpenFileDialogFragment.java
@@ -1,7 +1,6 @@
 package cooldomainname.com.the_best_text_editor_ever;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.*;
@@ -9,18 +8,18 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class SaveFileDialogFragment extends DialogFragment implements TextView.OnEditorActionListener {
+public class OpenFileDialogFragment extends DialogFragment implements TextView.OnEditorActionListener {
     private static String DEFAULT_TITLE = "Filename";
     private EditText mEditText;
 
-    public SaveFileDialogFragment() {
+    public OpenFileDialogFragment() {
         // Empty constructor is required for DialogFragment
         // Make sure not to add arguments to the constructor
         // Use `newInstance` instead as shown below
     }
 
-    public static SaveFileDialogFragment newInstance(String title) {
-        SaveFileDialogFragment frag = new SaveFileDialogFragment();
+    public static OpenFileDialogFragment newInstance(String title) {
+        OpenFileDialogFragment frag = new OpenFileDialogFragment();
         Bundle args = new Bundle();
         args.putString("title", title);
         frag.setArguments(args);
@@ -53,7 +52,7 @@ public class SaveFileDialogFragment extends DialogFragment implements TextView.O
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_dialog_filename, container);
     }
@@ -66,8 +65,8 @@ public class SaveFileDialogFragment extends DialogFragment implements TextView.O
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
         if (actionId == EditorInfo.IME_ACTION_DONE) {
             // Return input text back to activity through the implemented listener
-            SaveFileDialogListener listener = (SaveFileDialogListener) getActivity();
-            listener.onFinishEditSaveDialog(mEditText.getText().toString());
+            OpenFileDialogFragment.OpenFileDialogListener listener = (OpenFileDialogFragment.OpenFileDialogListener) getActivity();
+            listener.onFinishEditOpenDialog(mEditText.getText().toString());
             // Close the dialog and return back to the parent activity
             dismiss();
             return true;
@@ -76,8 +75,9 @@ public class SaveFileDialogFragment extends DialogFragment implements TextView.O
     }
 
     // 1. Defines the listener interface with a method passing back data result.
-    public interface SaveFileDialogListener {
-        void onFinishEditSaveDialog(String inputText);
+    public interface OpenFileDialogListener {
+        void onFinishEditOpenDialog(String inputText);
 
     }
+
 }
